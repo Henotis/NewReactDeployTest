9f8a839eee864ec2786600c2e6f368cc68ead999
Henry <henry.li@sesame.org>
2022-07-07 19:03:06 -0400

    2nd push

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaUsers} from 'react-icons/fa'
+import {BsFolderFill} from 'react-icons/bs'
 
-const about = () => {
+const About = () => {
   return (
-    <div>about</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>  
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaUsers className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ years</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Clients</h5>
+              <small>many</small>
+            </article>
+
+            <article className='about__card'>
+              <BsFolderFill className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ completed</small>
+              </article>
+          </div>
+          <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore perferendis maiores, eius ex fugiat minus, culpa autem dolore ullam quia doloribus adipisci mollitia voluptas! Architecto iure laboriosam quo animi rerum.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default about
+export default About
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './nav.css'
 import {AiFillHome} from 'react-icons/ai'
+import {BsFillPersonBadgeFill} from 'react-icons/bs'
+import {MdOutlineWork} from 'react-icons/md'
+import {BsHammer} from 'react-icons/bs'
+import {RiMessageFill} from 'react-icons/ri'
+import { useState } from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFillPersonBadgeFill /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWork /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BsHammer /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessageFill /></a>
     </nav>
   )
 }
